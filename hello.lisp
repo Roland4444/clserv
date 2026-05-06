abf331e4cc7d52746155affef7db9985fcb41df3
Roland4444 <turbopollux@rambler.ru>
2026-05-06 16:37:37 +0400

    enable thread

--- hello.lisp
+++ hello.lisp
@@ -1807,25 +1807,25 @@
 (setf hunchentoot:*session-max-time* (* 60 60 24)) ; 24 часа
 (setf hunchentoot:*session-gc-frequency* 60)
   ;; Запускаем фоновый поток для обработки заявок
-; (bt:make-thread
-;   (lambda ()
-;     (loop
-;       (sleep 10)
-;       (handler-case
-;           (when (gethash :processing-enabled *config*)
-;             (scan-requests))
-;         (error (e)
-;           (format t "~%!!! Критическая ошибка в потоке обработки: ~A. Поток продолжает работу.~%" e)
-;           (log-error-to-file e)
-;           (finish-output nil)))))
-;   :name "request-processor")
-  ; (bt:make-thread
-  ;   (lambda ()
-  ;     (loop
-  ;       (sleep 10)   ; интервал сканирования
-  ;       (when (gethash :processing-enabled *config*)
-  ;         (scan-requests))))
-  ;   :name "request-processor")
+(bt:make-thread
+  (lambda ()
+    (loop
+      (sleep 10)
+      (handler-case
+          (when (gethash :processing-enabled *config*)
+            (scan-requests))
+        (error (e)
+          (format t "~%!!! Критическая ошибка в потоке обработки: ~A. Поток продолжает работу.~%" e)
+          (log-error-to-file e)
+          (finish-output nil)))))
+  :name "request-processor")
+  (bt:make-thread
+    (lambda ()
+      (loop
+        (sleep 10)   ; интервал сканирования
+        (when (gethash :processing-enabled *config*)
+          (scan-requests))))
+    :name "request-processor")
   
   ;; Запуск сервера
   (start-server)
